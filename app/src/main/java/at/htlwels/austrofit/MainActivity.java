a71644504285ee0ecaa310c4d17c0009e7321eb0
Usama Youssef <usama.youssef@htl-wels.at>
2024-06-02 15:52:41 +0200

    AustroFit-commmit

--- app/src/main/java/at/htlwels/austrofit/MainActivity.java
+++ app/src/main/java/at/htlwels/austrofit/MainActivity.java
@@ -1,6 +1,8 @@
 package at.htlwels.austrofit;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.widget.TabHost;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,6 +17,29 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        TabHost th = (TabHost) findViewById(R.id.tabHost);
+        th.setup();
+        TabHost.TabSpec specs = th.newTabSpec("Tag1");
+        specs.setContent(R.id.tabPlan);
+        specs.setIndicator("Planer");
+        th.addTab(specs);
+
+        specs = th.newTabSpec("Tag2");
+        specs.setContent(R.id.tabKalorien);
+        specs.setIndicator("Tracken");
+        th.addTab(specs);
+
+        specs = th.newTabSpec("Tag3");
+        specs.setContent(R.id.tabZaehler);
+        specs.setIndicator("Schrittzähler");
+        th.addTab(specs);
+
+        specs = th.newTabSpec("Tag4");
+        specs.setContent(R.id.tabZiele);
+        specs.setIndicator("Ziele");
+        th.addTab(specs);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
