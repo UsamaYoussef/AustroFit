ac9fd5dd9e9c0ca860912b637f6c4b519c5f7bde
Alexander Soellinger <alexander.soellinger@htl-wels.at>
2024-06-18 13:22:10 +0200

    commit01 commit02

--- app/src/main/java/at/htlwels/austrofit/MainActivity.java
+++ app/src/main/java/at/htlwels/austrofit/MainActivity.java
@@ -17,11 +17,12 @@ public class MainActivity extends AppCompatActivity implements GoalAdapter.OnGoa
     private GoalAdapter goalAdapter;
     private List<Goal> goalList;
     private int selectedGoalPosition = -1;
+    private EditText newGoalEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_zielsetzer);
+        setContentView(R.layout.zielsetzer);
 
         goalList = new ArrayList<>();
         recyclerView = findViewById(R.id.recycler_view);
@@ -32,11 +33,17 @@ public class MainActivity extends AppCompatActivity implements GoalAdapter.OnGoa
         Button addButton = findViewById(R.id.button_add);
         Button deleteButton = findViewById(R.id.button_delete);
         Button editButton = findViewById(R.id.button_edit);
+        newGoalEditText = findViewById(R.id.newGoal);
 
         addButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                showGoalDialog(false);
+                String goalName = newGoalEditText.getText().toString();
+                if (!goalName.isEmpty()) {
+                    goalList.add(new Goal(goalName, false));
+                    goalAdapter.notifyItemInserted(goalList.size() - 1);
+                    newGoalEditText.setText("");
+                }
             }
         });
 
@@ -55,32 +62,24 @@ public class MainActivity extends AppCompatActivity implements GoalAdapter.OnGoa
             @Override
             public void onClick(View v) {
                 if (selectedGoalPosition >= 0) {
-                    showGoalDialog(true);
+                    showEditGoalDialog();
                 }
             }
         });
     }
 
-    private void showGoalDialog(final boolean isEdit) {
+    private void showEditGoalDialog() {
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setTitle(isEdit ? "Edit Goal" : "Add Goal");
+        builder.setTitle("Edit Goal");
 
         final EditText input = new EditText(this);
+        input.setText(goalList.get(selectedGoalPosition).getName());
         builder.setView(input);
 
-        if (isEdit && selectedGoalPosition >= 0) {
-            input.setText(goalList.get(selectedGoalPosition).getName());
-        }
-
-        builder.setPositiveButton(isEdit ? "Edit" : "Add", (dialog, which) -> {
+        builder.setPositiveButton("Edit", (dialog, which) -> {
             String goalName = input.getText().toString();
-            if (isEdit && selectedGoalPosition >= 0) {
-                goalList.get(selectedGoalPosition).setName(goalName);
-                goalAdapter.notifyItemChanged(selectedGoalPosition);
-            } else {
-                goalList.add(new Goal(goalName, false));
-                goalAdapter.notifyItemInserted(goalList.size() - 1);
-            }
+            goalList.get(selectedGoalPosition).setName(goalName);
+            goalAdapter.notifyItemChanged(selectedGoalPosition);
         });
         builder.setNegativeButton("Cancel", (dialog, which) -> dialog.cancel());
 
